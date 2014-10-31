ba81edb462aac9e02089cd4e919d184dd57ff9c5
Darren Wood <darren.wood@10gen.com>
2014-10-31 14:08:18 +1100

    change output path to use job id extension

--- bin/graph_output.sh
+++ bin/graph_output.sh
@@ -4,7 +4,7 @@ die () {
     exit 1
 }
 
-[ "$#" -eq 1 ] || die "Please provide output bucket name as an argument"
+[ "$#" -eq 1 ] || die "Please provide a single EMR JobId as an argument"
 : ${MRLOADER_S3_ROOT:?"Please set MRLOADER_S3_ROOT to valid S3 bucket. e.g s3://my_loader_bucket"}
 
 TEMP_DIR=./temp/$1/out
--- src/main/java/com/mongodb/mrloader/MapperClient.java
+++ src/main/java/com/mongodb/mrloader/MapperClient.java
@@ -1,5 +1,12 @@
 package com.mongodb.mrloader;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Scanner;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -8,13 +15,19 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
+
 import static com.mongodb.mrloader.LoaderConfiguration.*;
 
 public class MapperClient {
 
 	private static final String DEFAULT_JOB_NAME = "MongoDBLoader-" + System.currentTimeMillis();
+	private static final String JOB_FLOW_PATH = "/mnt/var/lib/info/job-flow.json";
+	private static final String JOB_FLOW_ID_KEY = "jobFlowId";
 
 	public static void main(String[] args) throws Exception {
+		
 		
 		// Convert the args to a job configuration
 		Configuration jobConfig = LoaderConfiguration.parseCommandLine(args);
@@ -22,6 +35,12 @@ public class MapperClient {
 		// If discovery enabled find the mongos list from the supplied URI
 		ConnectionManager.configureMongoConnection(jobConfig);	
 		
+		// Get the unique ID for this job
+		String jobId = getJobId();
+		
+		// if 'mapred_job_id' in os.environ:
+		//     jobID = os.environ['mapred_job_id']
+		    		
 		// Create the job and configure jar from this class
 		Job job = new Job(jobConfig, jobConfig.get(CFG_JOB_NAME, DEFAULT_JOB_NAME));
 		job.setJarByClass(MapperClient.class);
@@ -40,10 +59,29 @@ public class MapperClient {
 
 		// Setup the input and output S3 locations
 		FileInputFormat.addInputPath(job, new Path(jobConfig.get(CFG_INPUT_URI)));
-		FileOutputFormat.setOutputPath(job, new Path(jobConfig.get(CFG_OUTPUT_URI)));
+		
+		String outputUri = jobConfig.get(CFG_OUTPUT_URI);
+		if(jobId != null){ outputUri += jobId + "/"; }
+		System.out.println("Sending output to : " + outputUri);
+		FileOutputFormat.setOutputPath(job, new Path(outputUri));
 
 		// Execute job and wait
 		boolean result = job.waitForCompletion(true);
 		System.exit(result ? 0 : 1);
 	}
-}
+
+	
+	private static String getJobId() {
+		try{
+			// Read the job_flow json file from the node
+        	String content = new Scanner(new File(JOB_FLOW_PATH)).useDelimiter("\\Z").next();
+        	DBObject json = (DBObject) JSON.parse(content);
+        	return (String) json.get(JOB_FLOW_ID_KEY);
+        	
+		} catch(Exception e){}
+
+		return null;
+	}
+}			
+	
+	
